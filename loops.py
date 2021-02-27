8c0cc5c915456b470d4316fa4b00e19238d03efe
GerlinedP <gerlinedepater@hotmail.nl>
2021-02-27 20:02:19 +0100

    Ik heb rondjes gedraaid

---
+++ loops.py
@@ -0,0 +1,6 @@
+namen = ["kees", "piet", "amalia", "pieter-jan", 4, 4.5, False]
+
+for naam in namen:
+	print("Hallo {} {}".format(naam, ":D"))
+	print("Je bent van het type: {}".format(type(naam)))
+	print()
